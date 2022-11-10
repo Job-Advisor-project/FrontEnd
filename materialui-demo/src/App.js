6d2ca650bdc4dd48936c94cfc05fafe4fe015ea8
minjupgeorge <minjupgeorge>
2022-11-10 19:21:39 +0100

    react file added

---
+++ materialui-demo/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Demo from "./Demo";
+
+function App() {
+  return (
+    <div className="App">
+      <Demo />
+    </div>
+  );
+}
+
+export default App;
---
+++ materialui-demo/src/Demo.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+function Demo() {
+  return <Button variant="contained">Hello World</Button>;
+}
+export default Demo;
