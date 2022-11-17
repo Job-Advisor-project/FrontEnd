ffe5da4f6b4e7a847d27fa3dfbac41064bcfb6a9
Maria <naplesaries2014@gmail.com>
2022-11-17 10:19:12 +0100

    api calls with design by Fenny

---
+++ react/src/api/companiesByTag.js
@@ -0,0 +1,12 @@
+/**
+ * Fetch data about companies based on selected tag
+ * @param {input} string - Selected tag. Needed for api call
+ * @returns {obj} List of all companies that have info about selected tag in backend
+ **/
+export async function getCompaniesByTag(input) {
+  let url = `http://localhost:1337/api/${input}?fields[0]=id&populate[company][populate][image][fields][0]=formats`;
+  const response = await fetch(url);
+  const result = await response.json();
+  const data = result.data;
+  return data;
+}
--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import Carousel from "../components/Carousel";
 import Cards from "../components/CompanyCards";
+import { useEffect, useState } from "react";
+import { getCompaniesByTagVm } from "../api/viewModelels/companiesByTagVm";
 
-export default function Homepage({ companies }) {
-  return <Cards companies={companies}></Cards>;
+export default function Homepage() {
+  const [input, setInput] = useState("");
+  const [companyList, setCompanyList] = useState([]);
+  const handleInput = (section) => {
+    console.log(section);
+    setInput(section);
+  };
+  useEffect(() => {
+    getCompaniesByTagVm(input).then((vm) => {
+      setCompanyList(vm);
+    });
+  }, [input]);
+  return (
+    <>
+      <Carousel onInput={handleInput}></Carousel>
+      <Cards companies={companyList}></Cards>
+    </>
+  );
 }
