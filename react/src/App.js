5f7071dc2cd345a7a3e6c02b93f0adca6d99a4ff
minjupgeorge <minjupgeorge>
2022-11-18 17:44:38 +0100

    routers added to app and index

--- react/src/App.js
+++ react/src/App.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Home";
 
 // import SearchResult from "./pages/SearchResult";
@@ -7,6 +8,9 @@ export default function App() {
   return (
     <>
       <Homepage></Homepage>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
       {/* <SearchResult></SearchResult> */}
     </>
   );
