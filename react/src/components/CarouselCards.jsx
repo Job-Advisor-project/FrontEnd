be1fc1313651a5079f21bcebcc4bc169cfe2dee2
Maria <naplesaries2014@gmail.com>
2022-11-20 17:07:03 +0100

    added code for routers and package.json config for carousel

--- react/src/components/CarouselCards.jsx
+++ react/src/components/CarouselCards.jsx
@@ -5,7 +5,7 @@ import Item from "./Item";
 
 export default function CarouselSlide({ onSelect }) {
   return (
-    <Carousel>
+    <Carousel fade={true} pause={false} interval={2000}>
       {slider.map((item) => (
         <Item key={item.id} item={item} onSelect={onSelect} />
       ))}
