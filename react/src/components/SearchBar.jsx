6c3acdc575d59050ef1da08f63f69dab208e07aa
Maria <naplesaries2014@gmail.com>
2022-11-21 11:45:00 +0100

    added functional component SearchBar and styling

---
+++ react/src/components/SearchBar.jsx
@@ -0,0 +1,40 @@
+import { Button } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
+
+export const SearchBar = ({ setSearchQuery }) => (
+  <form style={{ textAlign: "center" }}>
+    <TextField
+      sx={{
+        textAlign: "center",
+        verticalAlign: "middle",
+        fontSize: "2",
+        width: "400px",
+      }}
+      id="search-bar"
+      className="text"
+      onInput={(e) => {
+        setSearchQuery(e.target.value);
+      }}
+      label="Search by company name..."
+      variant="outlined"
+      placeholder="Search..."
+      size="small"
+    />
+    <IconButton type="submit" aria-label="search">
+      <Button
+        style={{ backgroundColor: "#5A85C2", fontWeight: "600" }}
+        variant="contained"
+      >
+        Search
+      </Button>
+    </IconButton>
+  </form>
+);
+// export const filterData = (query, data) => {
+//   if (!query) {
+//     return data;
+//   } else {
+//     return data.filter((d) => d.toLowerCase().includes(query));
+//   }
+// };
--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
 import CarouselSlide from "../components/CarouselCards";
+// import { filterData, SearchBar } from "../components/SearchBar";
+import { SearchBar } from "../components/SearchBar";
+
 export default function Homepage({ onSelect }) {
+  const [searchQuery, setSearchQuery] = useState("");
+  // const dataFiltered = filterData(searchQuery, data);
+
   return (
     <>
+      <div
+        style={{
+          display: "flex",
+          alignSelf: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          padding: "20px",
+        }}
+      >
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        <div style={{ padding: 3 }}>
+          {searchQuery}
+          {/* {dataFiltered.map((d) => (
+            <div
+              className="text"
+              style={{
+                padding: 5,
+                justifyContent: "normal",
+                fontSize: 20,
+                color: "blue",
+                margin: 1,
+                width: "250px",
+                BorderColor: "green",
+                borderWidth: "10px",
+              }}
+              key={d.id}
+            >
+              {d}
+            </div>
+          ))} */}
+        </div>
+      </div>
       <CarouselSlide onSelect={onSelect}></CarouselSlide>
     </>
   );
