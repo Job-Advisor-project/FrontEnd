b1155d7d58af810d967a6c4218e390fd56c3905a
Maria <naplesaries2014@gmail.com>
2022-11-21 14:54:13 +0100

    added title for search by company name

--- react/src/components/TitleTag.jsx
+++ react/src/components/TitleTag.jsx
@@ -1,5 +1,11 @@
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 
 export default function TitleTag({ tag }) {
   const Div = styled("div")(({ theme }) => ({
@@ -7,12 +13,34 @@ export default function TitleTag({ tag }) {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(1),
   }));
-  return (
-    <Box sx={{ display: "flex" }}>
-      <Div
-        sx={{ alignSelf: "center" }}
-        component="div"
-      >{`Top companies for ${tag}`}</Div>
+  let title;
+  let theme = createTheme();
+  theme = responsiveFontSizes(theme);
+  tag === "work-life-balances" && (title = "Work-Life Balance");
+  tag === "diversity-inclusions" && (title = "Diversity & Inclusion");
+  tag === "benefits-compensations" && (title = "Benefits and Compensations");
+  tag === "social-responsibilities" && (title = "Sustainability");
+  return title === undefined ? (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <Typography variant="h4">Explore companies in Belgium</Typography>
+        <Typography variant="h5">
+          Job Advisor has many company overview. Find best one for you!
+        </Typography>
+      </ThemeProvider>
+    </Box>
+  ) : (
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Div sx={{ alignSelf: "center" }} component="div">
+        {`Top companies for `}
+        <span style={{ fontWeight: "bold" }}>{title}</span>
+      </Div>
       <img
         src="/./assets/figma download/pixeltrue-giveaway-1.png"
         alt="img"
