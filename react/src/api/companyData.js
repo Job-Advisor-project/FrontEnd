2ce18a677ede91a4b5bc699e50d71eb3bb11f976
Maria <naplesaries2014@gmail.com>
2022-11-24 22:48:30 +0100

    changed query to display all info

--- react/src/api/companyData.js
+++ react/src/api/companyData.js
@@ -1,5 +1,5 @@
 export async function getCompanyData(input) {
-  let url = `http://localhost:1337/api/companies?filters[name][$eq]=${input}`;
+  let url = `http://localhost:1337/api/companies?filters[name][$eq]=${input}&populate=*`;
   const response = await fetch(url);
   const result = await response.json();
   const data = result.data;
