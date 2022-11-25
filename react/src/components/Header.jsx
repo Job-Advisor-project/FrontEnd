f798708b150eb1e975c4ccff3bd9539c4803d27b
minjupgeorge <minjupgeorge>
2022-11-25 18:58:27 +0100

    Companycardbysearchcomponent added

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -6,31 +6,55 @@ import { useNavigate } from "react-router-dom";
 const Header = () => {
   const navigate = useNavigate();
   return (
-    <Box
-      sx={{
-        alignItems: "stretch",
-        backgroundImage: `url('${process.env.PUBLIC_URL}/assets/background.png')`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        height: "auto",
-        maxWidth: "100%",
-        fontSize: "0.875rem",
-        fontWeight: "700",
-      }}
-    >
-      <Grid item xs={6} md={4}>
-        <Typography sx={{ ml: 4 }}>
-          <img
-            src="../assets/ja-logo.png"
-            alt="jobadvisorlogo"
-            width={170}
-            height={150}
-            onClick={() => navigate("/")}
-          />
-        </Typography>
-      </Grid>
-    </Box>
+    <div>
+      <AppBar
+        position="flex"
+        sx={{
+          display: "flex",
+          justifyContent: "right",
+          margin: "0px",
+          backgroundImage: `url('${process.env.PUBLIC_URL}/assets/background.png')`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          height: "100",
+        }}
+      >
+        <Toolbar>
+          <Typography>
+            <img
+              src="../assets/jobadvisorlogo.jpg"
+              alt="jobadvisorlogo"
+              width={140}
+              height={80}
+            />
+          </Typography>
+          <Tabs
+            sx={{ marginLeft: "auto" }}
+            textcolor="inherit"
+            value={value}
+            onChange={(e, value) => setValue(1)}
+            indicatorvalue="primary"
+          >
+            <RateReviewIcon
+              sx={{
+                display: { xs: "none", md: "flex", color: "black" },
+                mr: -2,
+              }}
+            />
+            <Tab label="Reviews" />
+            <BusinessIcon
+              //color="action"
+              sx={{
+                display: { xs: "none", md: "flex", color: "black" },
+
+                mr: -2,
+              }}
+            />
+            <Tab label="Companies" />
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
--- react/src/pages/CompanyOverview.jsx
+++ react/src/pages/CompanyOverview.jsx
@@ -1,55 +1,30 @@
-import Footer from "../components/footer/Footer";
-import BasicTabs from "../components/Tabs";
-import { Box, Typography } from "@mui/material";
+//import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { getCompanyDataVm } from "../api/viewModelels/companyDataVm";
+import { Button, Container } from "@mui/material";
+import CompanycardsBysearch from "../components/CompanycardsBysearch";
 import { useNavigate } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function CompanyOverview({ company }) {
   const navigate = useNavigate();
+  const [companyData, setCompanyData] = useState([]);
+  useEffect(() => {
+    getCompanyDataVm(company).then((vm) => {
+      setCompanyData(vm);
+    });
+  }, [company]);
   return (
-    <Box
-      sx={{
-        alignItems: "stretch",
-        backgroundImage: `url('${process.env.PUBLIC_URL}/assets/background.png')`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        height: "auto",
-        maxWidth: "100%",
-        fontSize: "0.875rem",
-        fontWeight: "700",
-      }}
-    >
-      <Typography sx={{ ml: 4 }}>
-        <img
-          src="../assets/ja-logo.png"
-          alt="jobadvisorlogo"
-          width={170}
-          height={150}
-          onClick={() => navigate("/")}
-        />
-      </Typography>
-      <Box
-        sx={{
-          flexDirection: "column",
-          display: "flex",
-          alignItems: "stretch",
-          mx: 10,
-          mt: 3,
-          bgcolor: (theme) =>
-            theme.palette.mode === "dark" ? "#101010" : "#fff",
-          color: (theme) =>
-            theme.palette.mode === "dark" ? "grey.300" : "grey.800",
-          border: "1px solid",
-          borderColor: (theme) =>
-            theme.palette.mode === "dark" ? "grey.800" : "grey.300",
-          borderRadius: 2,
-          fontSize: "0.875rem",
-          fontWeight: "700",
-        }}
+    <Container>
+      <Header></Header>
+      <CompanycardsBysearch companies={companyData}></CompanycardsBysearch>
+      <Button
+        style={{ margin: "15px" }}
+        onClick={() => navigate("/")}
+        variant="contained"
       >
-        <BasicTabs company={company}></BasicTabs>
-      </Box>
-      <Footer></Footer>
-    </Box>
-  );
+        Back
+      </Button>
+    </Container>
+  ); //Company Card component might be added instead of <Typography>
 }
