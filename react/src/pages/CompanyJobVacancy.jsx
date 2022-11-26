73b069c6a954a5a9ced7a6ae8aa5586d0f3d777a
minjupgeorge <minjupgeorge>
2022-11-26 19:21:48 +0100

    box style added

--- react/src/pages/CompanyJobVacancy.jsx
+++ react/src/pages/CompanyJobVacancy.jsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getCompanyVacanciesVm } from "../api/viewModelels/companyVacancyVm";
 
@@ -9,7 +9,37 @@ export default function CompanyjobVacancy({ company }) {
       setCompanyVacancy(vm);
     });
   }, [company]);
+
   return companyVacancy.map((comp) => (
-    <Typography>{comp.attributes.title}</Typography>
+    <Box
+      sx={{
+        flexDirection: "column",
+        display: "flex",
+        alignItems: "stretch",
+        m: 5,
+        bgcolor: (theme) =>
+          theme.palette.mode === "dark" ? "#101010" : "#fff",
+        color: (theme) =>
+          theme.palette.mode === "dark" ? "grey.300" : "grey.800",
+        border: "1px solid",
+        borderColor: (theme) =>
+          theme.palette.mode === "dark" ? "grey.800" : "grey.300",
+        borderRadius: 2,
+        fontSize: "0.875rem",
+        fontWeight: "700",
+      }}
+    >
+      <Typography>
+        {comp.attributes.title}
+        <br></br>
+        {comp.attributes.description}
+        <br></br>
+        {comp.attributes.package}
+        <br></br>
+        {comp.attributes.contactEmail}
+        <br></br>
+        {comp.attributes.data}
+      </Typography>
+    </Box>
   )); //Company Card component might be added instead of <Typography>
 }
--- react/src/pages/CompanyOverview.jsx
+++ react/src/pages/CompanyOverview.jsx
@@ -1,10 +1,11 @@
-//import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getCompanyDataVm } from "../api/viewModelels/companyDataVm";
 //import VacanciesButton from "../components/VacanciesButton";
 //import CompanyjobVacancy from "./CompanyJobVacancy";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+//import Header from "../components/Header";
 
 export default function CompanyOverview({ company }) {
   const navigate = useNavigate();
@@ -15,20 +16,42 @@ export default function CompanyOverview({ company }) {
     });
   }, [company]);
   return companyData.map((comp) => (
-    //<Typography>{comp.attributes.name}</Typography>
-    <Button
-      style={{
-        backgroundColor: "#5A85C2",
-        fontWeight: "600",
-        marginLeft: "5px",
-      }}
-      variant="contained"
-      onClick={() => {
-        company = { company };
-        navigate("/companyVacancy");
-      }}
-    >
-      Search
-    </Button>
+    <div>
+      <Box
+        sx={{
+          flexDirection: "column",
+          display: "flex",
+          alignItems: "stretch",
+          m: 5,
+          bgcolor: (theme) =>
+            theme.palette.mode === "dark" ? "#101010" : "#fff",
+          color: (theme) =>
+            theme.palette.mode === "dark" ? "grey.300" : "grey.800",
+          border: "1px solid",
+          borderColor: (theme) =>
+            theme.palette.mode === "dark" ? "grey.800" : "grey.300",
+          borderRadius: 2,
+          fontSize: "0.875rem",
+          fontWeight: "700",
+        }}
+      >
+        <Typography>{comp.attributes.name}</Typography>
+      </Box>
+      <Button
+        style={{
+          alignItems: "flex-start",
+          backgroundColor: "#5A85C2",
+          fontWeight: "600",
+          marginLeft: "5px",
+        }}
+        variant="contained"
+        onClick={() => {
+          company = { company };
+          navigate("/companyVacancy");
+        }}
+      >
+        VACANCIES
+      </Button>
+    </div>
   )); //Company Card component might be added instead of <Typography>
 }
