67adea1062f61b9273f026b246f68c04c3e4f709
fenny wilriani <fenny.wilriani@gmail.com>
2022-11-26 11:54:07 +0100

    adding footer ja

--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "../components/Banner";
 import CarouselSlide from "../components/CarouselCards";
 import Header from "../components/Header";
-export default function Homepage({ onSelect, setCompany }) {
+import Footer from "../components/Footer";
+export default function Homepage({ onSelect }) {
   return (
     <>
-      <Header></Header>
-      <Banner setCompany={setCompany}></Banner>
+      <Header />
+      <Banner></Banner>
       <CarouselSlide onSelect={onSelect}></CarouselSlide>
+      <Footer />
     </>
   );
 }
