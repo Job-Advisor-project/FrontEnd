0895455808a0566c54cb26884ba82a4899bdd4ae
Maria <naplesaries2014@gmail.com>
2022-11-26 14:23:20 +0100

    npm checks

--- react/src/pages/CompanyOverview.jsx
+++ react/src/pages/CompanyOverview.jsx
@@ -1,14 +1,12 @@
 //import { Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getCompanyDataVm } from "../api/viewModelels/companyDataVm";
-import { Button, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import CompanycardsBysearch from "../components/CompanycardsBysearch";
-import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function CompanyOverview({ company }) {
-  const navigate = useNavigate();
   const [companyData, setCompanyData] = useState([]);
   useEffect(() => {
     getCompanyDataVm(company).then((vm) => {
