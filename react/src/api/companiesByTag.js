baf3e8f39bd58f5ada3b7c3d268a1b6c5fcb3553
Maria <naplesaries2014@gmail.com>
2022-11-27 15:21:54 +0100

    added config

--- react/src/api/companiesByTag.js
+++ react/src/api/companiesByTag.js
@@ -1,3 +1,5 @@
+import { settings } from "../config/config";
+
 /**
  * Fetch data about companies based on selected tag
  * @param {input} string - Selected tag. Needed for api call
@@ -5,7 +7,7 @@
  **/
 
 export async function getCompaniesByTag(tag) {
-  let url = `https://strapi-production-ed57.up.railway.app/api/${tag}?fields[0]=id&populate[company][populate][image][fields][0]=formats`;
+  let url = `${settings.BACKEND_URL}/api/${tag}?fields[0]=id&populate[company][populate][image][fields][0]=formats`;
   const response = await fetch(url);
   const result = await response.json();
   const data = result.data;
--- react/src/api/companyData.js
+++ react/src/api/companyData.js
@@ -1,19 +1,6 @@
-export async function getCompanyData(input, value) {
-  let url;
-  value === 0 &&
-    (url = `https://strapi-production-ed57.up.railway.app/api/companies?filters[name][$eq]=${input}&populate=image`);
-  value === 1 &&
-    (url = `https://strapi-production-ed57.up.railway.app/api/companies?filters[name][$eq]=${input}&populate[0]=image&populate[1]=benefits_compensations&populate=*`);
-  value === 2 &&
-    (url = `https://strapi-production-ed57.up.railway.app/api/companies?filters[name][$eq]=${input}&populate[0]=image&populate[1]=social_responsibilities.image`);
-  value === 3 &&
-    (url = `https://strapi-production-ed57.up.railway.app/api/companies?filters[name][$eq]=${input}&populate[0]=image&populate[1]=work_life_balances&populate=*`);
-
-  value === 4 &&
-    (url = `https://strapi-production-ed57.up.railway.app/api/companies?filters[name][$eq]=${input}&populate[0]=image&populate[1]=diversity_inclusions.image`);
-  console.log(url);
-  // value === 5 &&
-  //   (url = `https://strapi-production-ed57.up.railway.app/api/companies?filters[name][$eq]=${input}&populate[0]=diversity_inclusions&populate=*`);
+import { settings } from "../config/config";
+export async function getCompanyData(input) {
+  let url = `${settings.BACKEND_URL}/api/companies?filters[name][$eq]=${input}&populate=*`;
   const response = await fetch(url);
   const result = await response.json();
   const data = result.data;
