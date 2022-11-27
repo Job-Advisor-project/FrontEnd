01e6203beb2b867af7f42b679a3bbb080fb4b0c0
minjupgeorge <minjupgeorge>
2022-11-27 13:27:25 +0100

    header tabs added

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -1,12 +1,9 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import { Toolbar, Typography, Tabs, Tab } from "@mui/material";
-import BusinessIcon from "@mui/icons-material/Business";
-import RateReviewIcon from "@mui/icons-material/RateReview";
+import { Toolbar, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const [value, setValue] = useState();
   const navigate = useNavigate();
   return (
     <div>
@@ -27,35 +24,11 @@ const Header = () => {
             <img
               src="../assets/ja-logo.png"
               alt="jobadvisorlogo"
-              width={140}
-              height={80}
+              width={130}
+              height={100}
               onClick={() => navigate("/")}
             />
           </Typography>
-          <Tabs
-            sx={{ marginLeft: "auto" }}
-            textcolor="inherit"
-            value={value}
-            onChange={(e, value) => setValue(value)}
-            indicatorvalue="primary"
-          >
-            <RateReviewIcon
-              sx={{
-                display: { xs: "none", md: "flex", color: "black" },
-                mr: -2,
-              }}
-            />
-            <Tab label="Reviews" />
-            <BusinessIcon
-              //color="action"
-              sx={{
-                display: { xs: "none", md: "flex", color: "black" },
-
-                mr: -2,
-              }}
-            />
-            <Tab label="Companies" />
-          </Tabs>
         </Toolbar>
       </AppBar>
     </div>
