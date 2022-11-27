3f0a53fb6b126607c7c97fffce345286ad10ea79
minjupgeorge <minjupgeorge>
2022-11-27 16:28:19 +0100

    merged with master

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Aboutus from "./pages/Aboutus";
 import CompanyOverview from "./pages/CompanyOverview";
 import HomePage from "./pages/Home";
 import SearchResult from "./pages/SearchResult";
