296bf1b8bbb08cd9079c3a83a30a68d49164270d
fenny wilriani <fenny.wilriani@gmail.com>
2022-11-28 12:13:37 +0100

    updating search bar

--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import Banner from "../components/Banner";
 import CarouselSlide from "../components/CarouselCards";
-import Header from "../components/Header";
+//import Header from "../components/Header";
 import Footer from "../components/Footer";
 export default function Homepage({ onSelect }) {
   return (
     <>
-      <Header />
-      <Banner></Banner>
+      {/* <Header></Header> */}
+      <Banner setCompany={setCompany}></Banner>
       <CarouselSlide onSelect={onSelect}></CarouselSlide>
       <Footer />
     </>
