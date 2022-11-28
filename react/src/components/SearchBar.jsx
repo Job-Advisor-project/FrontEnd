c632ca2b1e21999f6cf7b1e1561f272a854d5d3a
fenny wilriani <fenny.wilriani@gmail.com>
2022-11-28 12:13:37 +0100

    updating search bar

--- react/src/components/SearchBar.jsx
+++ react/src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import { getCompanyListVm } from "../api/viewModelels/companyListVm";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 export default function SearchBar({ setCompany }) {
@@ -10,39 +10,49 @@ export default function SearchBar({ setCompany }) {
   const [data, setData] = useState([]);
   const [inputValue, setInputValue] = useState("");
   return (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
-      <Autocomplete
-        disablePortal
-        inputValue={inputValue}
-        onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        id="combo-box-demo"
-        options={data}
-        sx={{ alignSelf: "center" }}
-        renderInput={(params) => (
-          <TextField
-            sx={{ width: "494px" }}
-            {...params}
-            label="Search by company name..."
-            onClick={() => getCompanyListVm().then((v) => setData(v))}
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "stretch",
+        mt: 3,
+      }}
+    >
+      <Grid container justifyContent="left">
+        <Grid item xs={4} md={6} lg={8} sx={{ mr: 1 }}>
+          <Autocomplete
+            disablePortal
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            id="combo-box-demo"
+            options={data}
+            sx={{ alignSelf: "center" }}
+            renderInput={(params) => (
+              <TextField
+                sx={{ maxWidth: "100%" }}
+                {...params}
+                label="Search by company name..."
+                onClick={() => getCompanyListVm().then((v) => setData(v))}
+              />
+            )}
           />
-        )}
-      />
-      <Button
-        style={{
-          backgroundColor: "#5A85C2",
-          fontWeight: "600",
-          marginLeft: "5px",
-        }}
-        variant="contained"
-        onClick={() => {
-          setCompany(inputValue);
-          navigate("/companyOverview");
-        }}
-      >
-        Search
-      </Button>
+        </Grid>
+        <Button
+          style={{
+            backgroundColor: "#5A85C2",
+            fontWeight: "600",
+          }}
+          variant="contained"
+          onClick={() => {
+            setCompany(inputValue);
+            navigate("/companyOverview");
+          }}
+        >
+          Search
+        </Button>
+      </Grid>
     </Box>
   );
 }
--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import Banner from "../components/Banner";
 import CarouselSlide from "../components/CarouselCards";
-import Header from "../components/Header";
+//import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function Homepage({ onSelect, setCompany }) {
   return (
     <>
-      <Header></Header>
+      {/* <Header></Header> */}
       <Banner setCompany={setCompany}></Banner>
       <CarouselSlide onSelect={onSelect}></CarouselSlide>
       <Footer />
