347a5c5db562ae945db1e96baf72253c437e0701
fenny wilriani <fenny.wilriani@gmail.com>
2022-11-29 21:07:23 +0100

    changing

--- react/src/index.js
+++ react/src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import { ThemeProvider } from "@mui/material/styles";
-// import { theme } from "./pages/theme";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./pages/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <ThemeProvider theme={theme}>
-  <App />
-  // </ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
 );
--- react/src/pages/theme.js
+++ react/src/pages/theme.js
@@ -18,17 +18,7 @@ export const theme = createTheme({
   //   },
   // },
   typography: {
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
+    fontFamily: ["Poppins", "sans-serif"].join(","),
   },
+  h3: { fontWeight: 600 },
 });
