7eb79f63a93f6a5e7ac2a107486a1255308e3a68
fenny wilriani <fenny.wilriani@gmail.com>
2022-11-29 18:27:36 +0100

    update

--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Banner from "../components/Banner";
 import CarouselSlide from "../components/CarouselCards";
 //import Header from "../components/Header";
 import Footer from "../components/Footer";
-export default function Homepage({ onSelect }) {
+export default function Homepage({ onSelect, setCompany }) {
   return (
     <>
       {/* <Header></Header> */}
