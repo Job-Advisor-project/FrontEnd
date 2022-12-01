95ed2db9bcad25ea61ffa851689214d213662d7d
minjupgeorge <minjupgeorge>
2022-12-01 11:20:38 +0100

    value changed from 700 to 900

--- react/src/components/CarouselCards.jsx
+++ react/src/components/CarouselCards.jsx
@@ -12,12 +12,12 @@ export default function CarouselSlide({ onSelect }) {
       slidesToSlide: 1, // optional, default to 1.
     },
     tablet: {
-      breakpoint: { max: 1024, min: 700 },
+      breakpoint: { max: 1024, min: 900 },
       items: 2,
       slidesToSlide: 2, // optional, default to 1.
     },
     mobile: {
-      breakpoint: { max: 700, min: 0 },
+      breakpoint: { max: 900, min: 0 },
       items: 1,
       slidesToSlide: 1, // optional, default to 1.
     },
