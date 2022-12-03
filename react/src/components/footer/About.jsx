159a07acfa3cc3e1a1c1eb97c5a1d08d75829dd9
minjupgeorge <minjupgeorge>
2022-12-03 04:39:18 +0100

    vision changed

--- react/src/components/footer/About.jsx
+++ react/src/components/footer/About.jsx
@@ -1,6 +1,6 @@
-import { Box, Typography } from "@mui/material";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
+import { Box, Grid, Typography } from "@mui/material";
+//import Card from "@mui/material/Card";
+//import CardContent from "@mui/material/CardContent";
 
 export default function About() {
   return (
@@ -26,75 +26,77 @@ export default function About() {
           marginLeft: "3rem",
         }}
       >
-        <Typography
-          variant="h2"
-          style={{
-            textAlign: "center",
-            fontWeight: "700px",
-          }}
-          gutterBottom
-        >
-          JobAdvisor
-        </Typography>
-        <Typography
-          variant="paragraph"
-          style={{ textAlign: "center", marginBottom: "78px" }}
-          gutterBottom
-        >
-          We are here to help the motivated students and job seekers to
-          kickstart their career and also search their dream job without
-          registration.
-        </Typography>
-        <div>
-          <Card sx={{ maxWidth: 320 }}>
-            <CardContent>
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                align="center"
-              >
-                <h5>OUR MISSION</h5>
-              </Typography>
-              <Typography variant="paragraph" justify="" color="text.primary">
-                At Job Advisor, We help people to get jobs. We believe financial
-                independence can help people live up to their potential and get
-                back control of their own lives .
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card
+        <Grid item xs={6} md={4} sx={{ my: 8, mx: 2 }}>
+          <Typography
             sx={{
-              maxWidth: 320,
-              marginLeft: 50,
-              marginTop: -28,
-              marginBottom: 2,
+              ml: 4,
+              color: "#66cdaa",
+              marginTop: "50px",
             }}
+            variant="h2"
+            style={{
+              textAlign: "center",
+              fontWeight: "700px",
+            }}
+            gutterBottom
           >
-            <CardContent>
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                align="center"
-              >
-                <h5>OUR VISION</h5>
-              </Typography>
-              <Typography variant="paragraph" justify="" color="text.primary">
-                Create economic opportunity for every member of the global
-                workforce. our users can find all the information they need with
-                one or two clicks
-              </Typography>
-            </CardContent>
-          </Card>
+            About JobAdvisor
+          </Typography>
+        </Grid>
+        <Grid item md={5} lg={6} sx={{ my: 28, mx: 2, marginBottom: "28px" }}>
+          <Typography
+            variant="h5"
+            style={{ textAlign: "left", marginTop: "-5px" }}
+            gutterBottom
+          >
+            We are here to help the motivated students and job seekers to
+            kickstart their career and also search their dream job without
+            registration.
+          </Typography>
+        </Grid>
+        <div>
+          <Grid item md={5} lg={6} sx={{ my: 5, mx: 3 }}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              marginRight={"80px"}
+            >
+              MISSION
+            </Typography>
+          </Grid>
+          <Grid item md={5} lg={6} sx={{ my: 5, mx: 3 }}>
+            <Typography variant="paragraph" justify="" color="text.primary">
+              At Job Advisor, We help people to get jobs. We believe financial
+              independence can help people live up to their potential and get
+              back control of their own lives .
+            </Typography>
+          </Grid>
+
+          <Grid item md={5} lg={6} sx={{ my: 5, mx: 3 }}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              marginRight={"80px"}
+            >
+              VISION
+            </Typography>
+            <Typography variant="paragraph" justify="" color="text.primary">
+              Create economic opportunity for every member of the global
+              workforce. our users can find all the information they need with
+              one or two clicks
+            </Typography>
+          </Grid>
         </div>
       </div>
-
-      <img
-        src="../../assets/workpicture1.png"
-        alt="Team"
-        style={{ width: "652px", height: "410px", borderRadius: "90px" }}
-      />
+      <Grid item md={5} lg={12}>
+        <img
+          src="../../assets/workpicture1.png"
+          alt="Team"
+          style={{ width: "700px", height: "410px", borderRadius: "90px" }}
+        />
+      </Grid>
     </Box>
   );
 }
