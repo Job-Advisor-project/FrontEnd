d0794d92db61695a49fd79070a7f4e2d02fa6598
fenny wilriani <fenny.wilriani@gmail.com>
2022-12-04 13:53:06 +0100

    update cc

--- react/src/components/Jobs.jsx
+++ react/src/components/Jobs.jsx
@@ -1,12 +1,89 @@
+// import * as React from "react";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+// import CardMedia from "@mui/material/CardMedia";
+// import Typography from "@mui/material/Typography";
+// import { useState, useEffect } from "react";
+// import { getOpenJobsVm } from "../api/viewModelels/openJobsVm";
+// import Grid from "@mui/material/Grid";
+// import { Button, CardActionArea, CardActions } from "@mui/material";
+
+// export default function OpenJobs({ company }) {
+//   const openInNewTab = (url) => {
+//     window.open(url, "_blank", "noopener,noreferrer");
+//   };
+//   const [openJobs, setOpenJobs] = useState([]);
+//   useEffect(() => {
+//     getOpenJobsVm(company).then((vm) => {
+//       setOpenJobs(vm);
+//     });
+//   }, [company]);
+//   console.log(openJobs);
+//   return (
+//     <Grid container spacing={4}>
+//       {openJobs.map((job) =>
+//         job.attributes.job_opportunities.data.map((j) => (
+//           <Grid item xs={12} sm={12} md={4}>
+//             <Card
+//               key={j.attributes.title}
+//               sx={{ minWidth: 250, minHeight: 400, mt: 4 }}
+//             >
+//               <CardActionArea>
+//                 <CardMedia
+//                   key={j.attributes.title}
+//                   sx={{ objectFit: "cover", height: "20vh" }}
+//                   component="img"
+//                   image={`${job.attributes.image.data.attributes.formats.thumbnail.url}`}
+//                   alt="green iguana"
+//                 />
+//                 <CardContent>
+//                   <Typography gutterBottom variant="h5" component="div">
+//                     {j.attributes.title}
+//                   </Typography>
+//                   <Typography variant="body2" color="text.secondary">
+//                     {j.attributes.description}
+//                   </Typography>
+//                 </CardContent>
+//               </CardActionArea>
+//               <CardActions>
+//                 <Button
+//                   // href={`https://www.linkedin.com/company/${job.attributes.name}/jobs/`}
+//                   size="small"
+//                   color="primary"
+//                   target="_top"
+//                   rel="noopener noreferrer"
+//                   href={`mailto:${j.attributes.contactEmail}`}
+//                 >
+//                   Apply here
+//                 </Button>
+//                 <Button
+//                   onClick={() =>
+//                     openInNewTab(
+//                       `https://www.linkedin.com/company/${job.attributes.name}/jobs/`
+//                     )
+//                   }
+//                   size="small"
+//                   color="primary"
+//                 >
+//                   Learn more
+//                 </Button>
+//               </CardActions>
+//             </Card>
+//           </Grid>
+//         ))
+//       )}
+//     </Grid>
+//   );
+// }
+//=============================TRYOUT=====================================
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { useState, useEffect } from "react";
 import { getOpenJobsVm } from "../api/viewModelels/openJobsVm";
 import Grid from "@mui/material/Grid";
-import { Button, CardActionArea, CardActions } from "@mui/material";
+import { Button, CardActions, Avatar, CardHeader } from "@mui/material";
 
 export default function OpenJobs({ company }) {
   const openInNewTab = (url) => {
@@ -20,31 +97,31 @@ export default function OpenJobs({ company }) {
   }, [company]);
   console.log(openJobs);
   return (
-    <Grid container spacing={4}>
+    <Grid container spacing={2} justifyContent="center">
       {openJobs.map((job) =>
         job.attributes.job_opportunities.data.map((j) => (
-          <Grid item xs={12} sm={12} md={4}>
+          <Grid item sm={4} md={4}>
             <Card
               key={j.attributes.title}
               sx={{ minWidth: 250, minHeight: 400, mt: 4 }}
             >
-              <CardActionArea>
-                <CardMedia
-                  key={j.attributes.title}
-                  sx={{ objectFit: "cover", height: "20vh" }}
-                  component="img"
-                  image={`${job.attributes.image.data.attributes.formats.thumbnail.url}`}
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {j.attributes.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {j.attributes.description}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
+              <CardHeader
+                avatar={
+                  <Avatar
+                    variant="square"
+                    src={`${job.attributes.image.data.attributes.formats.thumbnail.url}`}
+                    sx={{ m: 1, width: 56, height: 56 }}
+                  ></Avatar>
+                }
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {j.attributes.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {j.attributes.description}
+                </Typography>
+              </CardContent>
               <CardActions>
                 <Button
                   // href={`https://www.linkedin.com/company/${job.attributes.name}/jobs/`}
--- react/src/pages/CompanyOverview.jsx
+++ react/src/pages/CompanyOverview.jsx
@@ -1,9 +1,66 @@
+// import Footer from "../components/footer/Footer";
+// import BasicTabs from "../components/Tabs";
+// import { Box, Typography } from "@mui/material";
+// import { useNavigate } from "react-router-dom";
+
+// export default function CompanyOverview({ company, setCompanyId }) {
+//   const navigate = useNavigate();
+//   return (
+//     <Box
+//       sx={{
+//         alignItems: "stretch",
+//         backgroundImage: `url('${process.env.PUBLIC_URL}/assets/background.png')`,
+//         backgroundRepeat: "no-repeat",
+//         backgroundPosition: "center",
+//         backgroundSize: "cover",
+//         height: "auto",
+//         maxWidth: "100%",
+//         fontSize: "0.875rem",
+//         fontWeight: "700",
+//       }}
+//     >
+//       <Typography sx={{ ml: 4 }}>
+//         <img
+//           src="../assets/ja-logo.png"
+//           alt="jobadvisorlogo"
+//           style={{ cursor: "pointer" }}
+//           width={170}
+//           height={150}
+//           onClick={() => navigate("/")}
+//         />
+//       </Typography>
+//       <Box
+//         sx={{
+//           flexDirection: "column",
+//           display: "flex",
+//           alignItems: "stretch",
+//           mx: 10,
+//           mt: 3,
+//           bgcolor: (theme) =>
+//             theme.palette.mode === "dark" ? "#101010" : "#fff",
+//           color: (theme) =>
+//             theme.palette.mode === "dark" ? "grey.300" : "grey.800",
+//           border: "1px solid",
+//           borderColor: (theme) =>
+//             theme.palette.mode === "dark" ? "grey.800" : "grey.300",
+//           borderRadius: 2,
+//           fontSize: "0.875rem",
+//           fontWeight: "700",
+//         }}
+//       >
+//         <BasicTabs company={company} setCompanyId={setCompanyId}></BasicTabs>
+//       </Box>
+//       <Footer></Footer>
+//     </Box>
+//   );
+// }
+//=================================TRYOUT==================================
 import Footer from "../components/footer/Footer";
 import BasicTabs from "../components/Tabs";
 import { Box, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
-export default function CompanyOverview({ company, setCompanyId }) {
+export default function CompanyOverview({ company }) {
   const navigate = useNavigate();
   return (
     <Box
@@ -29,27 +86,7 @@ export default function CompanyOverview({ company, setCompanyId }) {
           onClick={() => navigate("/")}
         />
       </Typography>
-      <Box
-        sx={{
-          flexDirection: "column",
-          display: "flex",
-          alignItems: "stretch",
-          mx: 10,
-          mt: 3,
-          bgcolor: (theme) =>
-            theme.palette.mode === "dark" ? "#101010" : "#fff",
-          color: (theme) =>
-            theme.palette.mode === "dark" ? "grey.300" : "grey.800",
-          border: "1px solid",
-          borderColor: (theme) =>
-            theme.palette.mode === "dark" ? "grey.800" : "grey.300",
-          borderRadius: 2,
-          fontSize: "0.875rem",
-          fontWeight: "700",
-        }}
-      >
-        <BasicTabs company={company} setCompanyId={setCompanyId}></BasicTabs>
-      </Box>
+      <BasicTabs company={company}></BasicTabs>
       <Footer></Footer>
     </Box>
   );
